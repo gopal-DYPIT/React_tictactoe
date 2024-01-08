bf88c277cfad57e2de1c28f6acd68ec62abcdd0c
gopal-DYPIT <gopalkadam212@gmail.com>
2024-01-08 23:28:26 +0530

    Initial add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Board from "./tic_tac_toe/board";
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
